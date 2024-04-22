9e31549b23b137ffe7da0e7c692157cd6bdc7b3d
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2024-04-22 16:12:03 +0700

    Create config..js

---
+++ config..js
@@ -0,0 +1,12 @@
+// Setting Bot
+global.botToken = 'Your Bot Token';
+global.api = 'https://itzpire.site';
+global.qrisImage = './qris.jpg'
+global.toxic = "true"; // true or false for simi
+
+// Setting owner
+global.ownerContact = {
+    phoneNumber: 'Your Number',
+    firstName: '',
+    lastName: '',
+  }
